64df428bc4429eca821e34aff5bcb949692cba3b
Sanpi <sanpi@homecomputing.fr>
2015-10-13 19:10:55 +0200

    Parse opening hours

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,21 +1,64 @@
+function getState(value)
+{
+    var state = '';
+
+    try {
+        var opening  = new opening_hours(value.tags.opening_hours, {
+            lat: location.lat,
+            lon: location.lon,
+            address: {
+                country_code: 'fr',
+            },
+        });
+
+        if (opening.getState()) {
+            state = 'open';
+        }
+        else {
+            state = 'closed';
+        }
+    }
+    catch (e) {
+    }
+
+    return state;
+}
+
 function SearchController($scope, $http)
 {
     $scope.search = function () {
-
         $http({
             url: 'http://nominatim.openstreetmap.org/search?format=json&q=' + $scope.where
         }).then(function success(response) {
+            var location = response.data[0];
+
             box = [
-                response.data[0].boundingbox[0],
-                response.data[0].boundingbox[2],
-                response.data[0].boundingbox[1],
-                response.data[0].boundingbox[3],
+                location.boundingbox[0],
+                location.boundingbox[2],
+                location.boundingbox[1],
+                location.boundingbox[3],
             ];
 
             $http({
                 url: 'http://overpass-api.de/api/interpreter?data=[out:json][timeout:25];node["opening_hours"](' + box.join() + ');out+body;'
             }).then(function sucess(response) {
-                $scope.objects = response.data.elements;
+                $scope.objects = [];
+
+                response.data.elements.forEach(function (value) {
+                    if (
+                        typeof value.tags.name == 'undefined'
+                        && typeof value.tags.amenity == 'undefined'
+                    ) {
+                        return;
+                    }
+
+                    $scope.objects.push({
+                        name: value.tags.name,
+                        amenity: value.tags.amenity,
+                        opening_hours: value.tags.opening_hours,
+                        state: getState(value),
+                    });
+                });
             });
         });
     };
