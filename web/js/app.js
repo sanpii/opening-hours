40c4faacd512317eb3092b649fa5b68f2c068ac6
Sanpi <sanpi@homecomputing.fr>
2015-10-14 18:44:22 +0200

    Add about page

--- web/js/app.js
+++ web/js/app.js
@@ -3,6 +3,10 @@
 var app = angular.module('opening-hours', ['ngRoute', 'ngResource']);
 
 app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/about', {
+        templateUrl: 'partials/about.html'
+    });
+
     $routeProvider.when('/:where?/:what?', {
         templateUrl: 'partials/search.html',
         controller: SearchController
